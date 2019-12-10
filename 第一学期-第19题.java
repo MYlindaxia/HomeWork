5e58b782a54d78f4489255e0c9a4b7fbe1b8a619
MYlindaxia <2444501319@qq.com>
2019-12-10 12:32:41 +0800

    增加了16.19题

---
+++ 第一学期-第19题.java
@@ -0,0 +1,15 @@
+package Code;
+
+public class Test1204 {
+	public static void main(String[] args) {
+		System.out.println("         *");
+		System.out.println("        ***");
+		System.out.println("      ******");
+		System.out.println("     ********");
+		System.out.println("      ******");
+		System.out.println("       ***");
+		System.out.println("        *");
+
+	}
+
+}
