3ba779d429beb58eacf975d7317835f2fdd48a00
MYlindaxia <57577728+MYlindaxia@users.noreply.github.com>
2019-12-11 17:07:11 +0800

    添加第23题

---
+++ 第一学期练习题答案（自己做的）/第一学期-第23题.java
@@ -0,0 +1,14 @@
+package Code;
+
+
+public class HomeWork {
+	public static void main(String[] args) {
+		int age = 10 ;
+		for(int timer = 1;timer<=4;timer++){
+			
+			 age += 2;
+		}
+		System.out.println(age);
+		
+	}
+}
