ccb8b3b8e0df8a2dad6984aa82cc16b4249fdc36
MYlindaxia <57577728+MYlindaxia@users.noreply.github.com>
2019-12-11 17:22:19 +0800

    添加第22题

---
+++ 第一学期练习题答案（自己做的）/第一学期-第22题.java
@@ -0,0 +1,16 @@
+package Code;
+
+
+public class HomeWork {
+	public static void main(String[] args) {
+		int sum1=1;
+for(int i = 1;i<=5;i++){
+	sum1 = sum1 * i;
+
+	
+}
+System.out.println(sum1);
+
+		
+	}
+}
