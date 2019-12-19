70bf6f80b2c1976c689a8fac1acf2ffceab85f13
MYlindaxia <2444501319@qq.com>
2019-12-19 22:57:57 +0800

    修改了第十九题的答案

--- 第一学期练习题答案（自己做的）/第一学期-第19题.java
+++ 第一学期练习题答案（自己做的）/第一学期-第19题.java
@@ -1,15 +1,24 @@
 package Code;
 
-public class Test1204 {
+public class jt1 {
 	public static void main(String[] args) {
-		System.out.println("         *");
-		System.out.println("        ***");
-		System.out.println("      ******");
-		System.out.println("     ********");
-		System.out.println("      ******");
-		System.out.println("       ***");
-		System.out.println("        *");
-
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= 4 - i; j++) {
+				System.out.print(' ');
+			}
+			for (int j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int z = 3; z >= 1; z--) {
+			for (int q = 1; q <= 4 - z; q++) {
+				System.out.print(' ');
+			}
+			for (int q = 1; q <= 2 * z - 1; q++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
-
 }
