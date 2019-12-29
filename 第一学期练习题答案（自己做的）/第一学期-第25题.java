19abbd8e13e14834965535223ac5320ac03ada4f
MYlindaxia <2444501319@qq.com>
2019-12-29 18:23:30 +0800

    优化了第25题的解法

--- 第一学期练习题答案（自己做的）/第一学期-第25题.java
+++ 第一学期练习题答案（自己做的）/第一学期-第25题.java
@@ -2,34 +2,18 @@ package Code;
 
 import java.util.Scanner;
 
-public class huiwenshu {
+public class Test21 {
 	public static void main(String[] args) {
-		System.out.println("请输入5位数来判断是否为回文数");
-		Scanner scan = new Scanner(System.in);
-		int i = scan.nextInt();
-		int w;
-		int q;
-		int b;
-		int s;
-		int g;
-		w = i/10000;
-		q = i%10000/1000;
-		b = i%10000%1000/100;
-		s = i%10000%1000%100/10;
-		g = i%10000%1000%100%10/1;
-		if(w==g){
-			if(q==s){
-				System.out.println("是回文数");
-			}else{
-				System.out.println("不是回文数");
-			}
-		}else{
+		System.out.println("请输入数字");
+		Scanner scan1 = new Scanner(System.in);
+		int i = scan1.nextInt();
+		String s = String.valueOf(i);
+		char[] c = s.toCharArray();
+		if (c[0] == c[4] && c[1] == c[3]) {
+			System.out.println("是回文数");
+		} else {
 			System.out.println("不是回文数");
-
 		}
-		
-		
-		
-	}
 
+	}
 }
